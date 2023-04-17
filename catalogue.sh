cd7ca8b6f627b8a994e0493e1cfa682e4f53ed3a
Raghu K <raghuk.vit@gmail.com>
2023-04-17 06:49:27 +0530

    Catalogue Changes

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ npm install
 cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
