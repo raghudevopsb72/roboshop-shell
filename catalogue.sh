7d7effeb8b5f39546829d4ad3f77eab2a9e0fabd
Raghu K <raghuk.vit@gmail.com>
2023-04-17 07:07:33 +0530

    Adding print statements

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<\e[0m"
 systemctl daemon-reload
