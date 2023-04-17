7346c6cb80de03d8efae41b87bd062daa8633677
Raghu K <raghuk.vit@gmail.com>
2023-04-17 07:28:38 +0530

    Addiing paths

--- redis.sh
+++ redis.sh
@@ -6,7 +6,7 @@ dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
 echo -e "\e[36m>>>>>>>>> Update Redis Listen Address <<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[36m>>>>>>>>> Start Redis Service <<<<<<<<\e[0m"
 systemctl enable redis
