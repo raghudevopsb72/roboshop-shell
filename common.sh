0b5026f7ca15c308a8e79c93c6355f1fca20ba33
Raghu K <raghuk.vit@gmail.com>
2023-04-20 07:17:24 +0530

    Shipping Changes

--- common.sh
+++ common.sh
@@ -114,3 +114,23 @@ func_java() {
   func_schema_setup
   func_systemd_setup
 }
+
+
+func_python() {
+  func_print_head "Install Python"
+  yum install python36 gcc python3-devel -y &>>$log_file
+  func_stat_check $?
+
+  func_app_prereq
+
+  func_print_head "Install Python Dependencies"
+  pip3.6 install -r requirements.txt &>>$log_file
+  func_stat_check $?
+
+  func_print_head "Update Passwords in System Service file"
+  sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service &>>$log_file
+  func_stat_check $?
+
+  func_systemd_setup
+
+}
--- payment.sh
+++ payment.sh
@@ -8,31 +8,5 @@ if [ -z "$rabbitmq_appuser_password" ]; then
   exit
 fi
 
-echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
-yum install python36 gcc python3-devel -y
-
-echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>> Create App Dir <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-
-echo -e "\e[36m>>>>>>>>> Extract App Content <<<<<<<<\e[0m"
-cd /app
-unzip /tmp/payment.zip
-
-echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
-pip3.6 install -r requirements.txt
-
-echo -e "\e[36m>>>>>>>>>Setup SystemD Service <<<<<<<<\e[0m"
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
-cp ${script_path}/payment.service /etc/systemd/system/payment.service
-
-echo -e "\e[36m>>>>>>>>> Start Payment Service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+component=payment
+func_python
